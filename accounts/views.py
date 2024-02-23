37ed370f4cbcba63a0e537ef2a3b32292b401e38
Abolfazl Hajsharifi <abolfaazlhajsherifi@gmail.com>
2024-02-23 16:11:13 +0330

    fixed some problem also added email sender for send verification code to user email

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,26 @@
+from email.message import EmailMessage
 from django.shortcuts import render,redirect,HttpResponse
 from django.views.generic import DetailView,View
+from django.contrib import messages
+from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth.views import LoginView
-from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth import login,authenticate,logout,get_user_model
 from django.core.exceptions import ValidationError
+from django.core import mail
 from django.views.generic import CreateView
 from django.urls import reverse,reverse_lazy
-
-
+from django.template.loader import render_to_string,get_template
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_decode,urlsafe_base64_encode
+from django.utils.html import strip_tags
+from base64 import urlsafe_b64encode
+from .tokens import account_activation_token
+from django.conf import settings
 from .models import User
 from .forms import UserForm,RegisterForm
 # Create your views here.
+
+user=get_user_model()
 
 class Login(View):
     template_name="accounts/Login.html"
@@ -50,20 +61,40 @@ class RegisterView(CreateView):
     form_class = RegisterForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
-    def post(self,request):
+    def get(self,request):
         if request.user.is_authenticated:
-            return redirect("login")
+            return redirect("accounts:login")
         else:
-                form=RegisterForm(request.POST)
-                if form.is_valid:
+            form=RegisterForm
+            return render(request,'accounts/signup.html',context={'form':form})
+        
+    def post(self,request):
+        email_from = settings.EMAIL_HOST_USER
+        subject="Welcome To Django Twitter"
+        form=RegisterForm(request.POST)
+        if form.is_valid:
                     new_user=form.save(commit=False)
                     new_user.is_active=False
                     new_user.save()
+                    # template=get_template("acc_active_email.html")
+                    token=account_activation_token.make_token(new_user)    
+                    uid = urlsafe_b64encode(force_bytes(user.pk))   
+                    current_site = get_current_site(request)
+                    context={
+                    'user': new_user,
+                    'domain': current_site.domain,
+                    'uid':uid,
+                    'token':token,}
+                    html_message = render_to_string('accounts/acc_active_email.html', context)     
+                    plain_message = strip_tags(html_message)
+                    # email= EmailMessage(subject,html_message,email_from,[new_user.email])
+                    mail.send_mail(subject,plain_message,email_from,[new_user.email],html_message=html_message)
+                    status=200
+                    return HttpResponse('check your email for verification check spam folder too')
+        else:
+                    messages.error(request, 'Error')
+                    status = 400
+                    return HttpResponse("ur register has an error")
                 
-                
-                    return HttpResponse("you are register check email for verification")
-                else:
-                    raise ValidationError("check your fields again")
-            
 def emailconfirm(request):
     return render(request,'signed_up.html')
