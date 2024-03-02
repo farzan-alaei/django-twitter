7f330d52c9b525ca1636ee313c38a2952d5852e7
Abolfazl Hajsharifi <abolfaazlhajsherifi@gmail.com>
2024-03-02 09:53:35 +0330

    email detail added

--- src/settings.py
+++ src/settings.py
@@ -124,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = False
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+
