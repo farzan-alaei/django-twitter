d65298d87f169d40beca51491b60387e453c2e5c
Abolfazl Hajsharifi <abolfaazlhajsherifi@gmail.com>
2024-03-02 09:51:21 +0330

    urls.py updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from django.urls import path
-from .views import Login,RegisterView
+from django.urls import path,include
+from .views import RegisterView,active_user,LoginView,UserProfileDetailView,user_info,UserUpdateView
+
 app_name='accounts'
+
 urlpatterns = [
-    path('login', Login.as_view(),name='login'),
-    path('register', RegisterView.as_view(),name='register')
-    
+    path('register/',RegisterView.as_view(),name='register'),
+    path('login/',LoginView.as_view(),name='login'),
+    path('activate/<uidb64>/<token>',active_user.as_view(),name='activate'),
+    path('profile/<int:pk>',UserProfileDetailView.as_view(),name='profile'),
+    path('update/<pk>',UserUpdateView.as_view(),name='update')
+    # path('profile/',user_info,name='profile')
+
 ]
