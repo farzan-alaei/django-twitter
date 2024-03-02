b19aee5490cd6028f434ce82b0a94118342006d3
Abolfazl Hajsharifi <abolfaazlhajsherifi@gmail.com>
2024-03-02 09:49:21 +0330

    user manage completely changed

--- accounts/models.py
+++ accounts/models.py
@@ -1,37 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser,PermissionsMixin
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.core.mail import send_mail
 from django.utils import timezone
-
+from django.contrib.auth.hashers import make_password
 
 class MyUserManager(BaseUserManager):
-    def _create_user(self,email,password,**others_fields):
-        if not email : 
-            raise ValueError("Users must have an email")
-        user=self.model(
-            email=self.normalize_email(email),**others_fields)
+    def create_user(self, email, password=None,**extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields,
+        )
+
         user.set_password(password)
-        others_fields.setdefault("is_staff", False)
-        others_fields.setdefault("is_superuser", False)
         user.save(using=self._db)
         return user
-    
-    def create_user(self, email, password=None,**others_fields):
-        others_fields.setdefault("is_staff", False)
-        others_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, **others_fields)
 
-    def create_superuser(self,  email, password=None, **others_fields):
-        others_fields.setdefault("is_staff", True)
-        others_fields.setdefault("is_superuser", True)
-        others_fields.setdefault("is_active",True)
-        if others_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if others_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-        return self._create_user(email, password, **others_fields)
+    def create_superuser(self, email, password=None,**extra_fields):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            **extra_fields,
+        )
+        user.is_staff = True
+        user.is_active = True
+        user.is_superuser=True
         
+        user.save(using=self._db)
+        return user        
     
 class User(AbstractBaseUser,PermissionsMixin):
     first_name = models.CharField (_("firstname"),max_length=150,blank=True)
@@ -42,6 +49,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     is_active=models.BooleanField(_('active'),default=False,help_text=_('show that user can log in to website or not!'))
     date_joined=models.DateTimeField(_("date_joined"),default=timezone.now)
     image=models.ImageField(_('image'),blank=True)
+    
     objects=MyUserManager()
     
     EMAIL_FIELD='email'
@@ -70,6 +78,10 @@ class User(AbstractBaseUser,PermissionsMixin):
     def email_user(self, subject, message, from_email=None, **kwargs):
         """Send an email to this user."""
         send_mail(subject, message, from_email, [self.email], **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse("accounts:update", kwargs={"pk": self.pk})
+    
 
 class UserFollowing(models.Model):
     
