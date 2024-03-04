75da0541fd1d90bc846ee55016ac182d3475c179
Abolfazl Hajsharifi <abolfaazlhajsherifi@gmail.com>
2024-03-04 12:25:08 +0330

    urls updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import RegisterView,active_user,LoginView,UserProfileDetailView,user_info,UserUpdateView
+from .views import RegisterView,active_user,LoginView,UserProfileDetailView,UserUpdateView
 
 app_name='accounts'
 
