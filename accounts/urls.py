b9c3fdee920693c1e227fd5c3a7bced715643bb0
Abolfazl Hajsharifi <abolfaazlhajsherifi@gmail.com>
2024-04-09 12:08:53 +0330

    new urls added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
 from .views import RegisterView,active_user,LoginView,UserProfileDetailView,UserUpdateView
+from django.conf.urls.static import static  
+from django.conf import settings
 
 app_name='accounts'
 
@@ -9,7 +11,10 @@ urlpatterns = [
     path('login/',LoginView.as_view(),name='login'),
     path('activate/<uidb64>/<token>',active_user.as_view(),name='activate'),
     path('profile/<int:pk>',UserProfileDetailView.as_view(),name='profile'),
-    path('update/<pk>',UserUpdateView.as_view(),name='update')
+    path('update/<pk>',UserUpdateView.as_view(),name='update'),
+
+    # path('profile/<int:pk>',follow_user,name='follow')
+
     # path('profile/',user_info,name='profile')
 
 ]
