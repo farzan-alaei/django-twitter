98af2c050d7ca14a311564509c5e85171975598f
Abolfazl Hajsharifi <abolfaazlhajsherifi@gmail.com>
2024-04-09 12:10:40 +0330

    new models added to  django admin

--- chatapp/admin.py
+++ chatapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Message,Room
 # Register your models here.
+
+   
+@admin.register(Room)
+class roomAdmin(admin.ModelAdmin):
+    list_display=['first_user','second_user','create_time','name']
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display=['room','sender','content']
+    
