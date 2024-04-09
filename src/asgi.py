b8030467c3791a5a8a7df060a1b8ffad8982702e
Abolfazl Hajsharifi <abolfaazlhajsherifi@gmail.com>
2024-04-09 12:06:39 +0330

    some conf for django channels

--- src/asgi.py
+++ src/asgi.py
@@ -10,7 +10,24 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter,URLRouter
+from chatapp.routing import websocket_urlpatterns
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+
+
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        'http':get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+        
+    }
+)
+
